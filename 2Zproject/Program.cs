bc3d09a5a31aa3288524c262356f86a85fc0af8e
Thomas Thorogood <thom562r@mail.hansenberg.dk>
2023-12-05 14:48:30 +0100

    crazy
    datatyper

--- 2Zproject/Program.cs
+++ 2Zproject/Program.cs
@@ -1,2 +1,23 @@
 ﻿
 Console.WriteLine("Hello, World!");
+int x;
+x = 4;
+int y= 5, z = 6;
+
+Console.WriteLine("x is "+x);
+Console.WriteLine("y is "+y);
+Console.WriteLine("z is "+z);
+
+float d =5.2F;
+Console.WriteLine("d is "+d);
+
+Console.WriteLine(d+x-z/y*d%x);
+int e = Convert.ToInt16(d);
+Console.WriteLine("Float to int "+e);
+
+
+
+String s = "yo yo yo what's up"
+
+Console.WriteLine(s);
+
