7209440950ecfe0e3ea8be0556c53d5849ebf93d
Thomas Thorogood <thom562r@mail.hansenberg.dk>
2023-12-12 12:09:11 +0100

    wooow
    crazy

--- 2Zproject/Program.cs
+++ 2Zproject/Program.cs
@@ -80,7 +80,9 @@ for (int i = width; i > 0; i--)
 }
 */
 
-int attempts = 0;
+using System.ComponentModel.DataAnnotations;
+
+
 
 int numberofrightanswers = 0;
 static void write(string a){
@@ -91,48 +93,146 @@ static void write(string a){
  Console.Write("\n");
 }
 
+
+int minlength = 0;
+int maxlength = 0;
+int y = 0;
+
 write("WELCOME TO THE PASSWORD GAME!");
 write("\nToday we will be testing your skills.");
-
+Thread.Sleep(1500);
 
 while(true)
 {
+ if(y == 0) {
 
- var rand = new Random().Next(100000,999999);
+  Console.Clear();
+  write("Choose your speed.");
+  write("\nSlow");
+  write("\nNormal");
+  write("\nFast");
+  write("\nDeath Mode\n");
+  
 
- write("The password will be shown in");
- Thread.Sleep(1000);
- Console.WriteLine("3");
- Thread.Sleep(1000);
- Console.WriteLine("2");
- Thread.Sleep(1000);
- Console.WriteLine("1");
- Thread.Sleep(1000);
- Console.WriteLine(rand);
- Thread.Sleep(1000);
- Console.Clear();
- write("What was the number?");
-
-string password = Console.ReadLine();
-
- if(password == rand.ToString())
- {
-  write("Thats correct!");
-  numberofrightanswers++;
+  
 
 
+  switch (Console.ReadLine().ToLower())
+  {
+   case "slow":
+   y = 2000;
+   break;
+   case "fast": 
+   y = 500;
+   break;
+   case "death mode": 
+   y = 250;
+   break;
+   case "normal":  
+   default:
+   y = 1000;
+   break;
+  
+  }
+  Console.Clear();
+
+  
+
+
+  write("Choose your length.");
+  write("\n3 digits");
+  write("\n5 digits");
+  write("\n7 digits");
+  write("\n9 digits\n");
+
+  switch (Console.ReadLine().ToLower())
+  {
+   case "3 digits":
+   minlength = 100;
+   maxlength = 999;
+   break;
+   case "7 digits": 
+   minlength = 1000000;
+   maxlength = 9999999;
+   break;
+   case "9 digits": 
+    minlength = 100000000;
+    maxlength = 999999999;
+   break;
+   case "5 digits":  
+   default:
+    minlength = 10000;
+    maxlength = 99999;
+   break;
+   
+  }
+  Console.Clear();
  }
 
- else
- {
-  write("\nThat was incorrect.\n\nYou got "+numberofrightanswers+ " right.");
-  numberofrightanswers = 0;
-  write("\nThe correct answer was "+ rand);
-  write("\nPress any key to try again.");
-  Console.ReadKey();
- }
+
+
+
+
+
+
+
+
+
+
+   var rand = new Random().Next(minlength,maxlength);
+
+   write("The password will be shown in");
+   Thread.Sleep(1000);
+   Console.WriteLine("3");
+   Thread.Sleep(1000);
+   Console.WriteLine("2");
+   Thread.Sleep(1000);
+   Console.WriteLine("1");
+   Thread.Sleep(1000);
+   Console.WriteLine(rand);
+   Thread.Sleep(y);
+   Console.Clear();
+   write("What was the number?");
+
+   string password = Console.ReadLine();
+
+  if(password == rand.ToString())
+  {
+   write("Thats correct!");
+   numberofrightanswers++;
+
+
+  }
+
+  else
+  {
+   Console.WriteLine("-------------------------------");
+   write("That was incorrect.\n\nYou got "+numberofrightanswers+ " right.");
+   
+   numberofrightanswers = 0;
+   write("\nThe correct answer was "+ rand);
+   Console.WriteLine("-------------------------------");
+   Console.ReadKey();
+   Console.Clear();
+   write("\n\nDo you want to try again?.");
+   write("\nYes");
+   write("\nNo");
+   string Retry = Console.ReadLine();
+   if(Retry.ToLower() == "yes")
+   {
+    Console.Clear();
+    continue;
+   
+   }
+   else
+   {
+    Console.Clear();
+    y = 0;
+   }
+
+  }
  
-
+ 
 
 
 
